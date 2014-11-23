916d9d1c80ff0e1bf6d6cb5e3ed61f5e8453243f
jordimontana82 <jordi.montana@gmail.com>
2014-11-23 20:06:19 +0100

    Initial commit

---
+++ FakeXrmEasy.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Xunit;
+namespace FakeXrmEasy.Tests
+{
+    public class FakeXrmEasyTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ FakeXrmEasy/FakeContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FakeXrmEasy
+{
+    /// <summary>
+    /// A fake context stores In-Memory entites indexed by logical name and then Guids
+    /// </summary>
+    public class FakeContext
+    {
+
+    }
+}
