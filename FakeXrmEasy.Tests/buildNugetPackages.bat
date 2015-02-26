c2c0fa03fc1d4c3a5c312a8f3d97df3f2622f060
jordimontana82 <jordi.montana@gmail.com>
2015-02-26 01:12:09 +0100

    Deployed a new version of the NuGet package

--- FakeXrmEasy.Tests/buildNugetPackages.bat
+++ FakeXrmEasy.Tests/buildNugetPackages.bat
@@ -1,4 +1,4 @@
 copy ..\FakeXrmEasy\bin\Debug\FakeXrmEasy.dll .\build\lib\net40
 cd build
 nuget pack FakeXrmEasy.dll.nuspec
-nuget push FakeXrmEasy.1.2.7-alpha.nupkg
+nuget push FakeXrmEasy.1.2.8-alpha.nupkg
