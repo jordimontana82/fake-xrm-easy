98bec441c19b280b72bfac959dd613d37d9fefef
jordimontana82 <jordi.montana@gmail.com>
2015-03-02 03:37:49 +0100

    Added tests for tracing service

---
+++ FakeXrmEasy.Tests/FakeContextTests/FakeTracingServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Xunit;
+using FakeItEasy;
+using FakeXrmEasy;
+
+using System.Collections.Generic;
+using Microsoft.Xrm.Sdk;
+using System.Linq;
+
+namespace FakeXrmEasy.Tests.FakeContextTests
+{
+    public class FakeTracingServiceTests
+    {
+        [Fact]
+        public void When_a_trace_is_dumped_it_should_return_right_traces()
+        {
+            var tracingService = new XrmFakedTracingService();
+
+            var trace1 = "This is one trace";
+            var trace2 = "This is a second trace";
+
+            tracingService.Trace(trace1);
+            tracingService.Trace(trace2);
+
+            Assert.Equal(tracingService.DumpTrace(), trace1 + trace2);
+        }
+    }
+}
+
