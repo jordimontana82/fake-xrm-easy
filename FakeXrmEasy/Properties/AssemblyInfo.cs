7cf65d11807daa47a7f2028e8901785beb5fcf9b
jordimontana82 <jordi.montana@gmail.com>
2015-03-14 15:52:52 +0100

    Updated nuget package version and releaseNotes. Added frameworkAssemblies definitions as well.

--- FakeXrmEasy/Properties/AssemblyInfo.cs
+++ FakeXrmEasy/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FakeXrmEasy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://it-gems.com/fake-xrm-easy.html")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FakeXrmEasy")]
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("1.2.4.0")]
-//[assembly: AssemblyFileVersion("1.2.4.0")]
-[assembly: AssemblyInformationalVersion("1.2.11-alpha")]
+//[assembly: AssemblyVersion("1.2.12.0")]
+//[assembly: AssemblyFileVersion("1.2.12.0")]
+[assembly: AssemblyInformationalVersion("1.2.12-alpha")]
