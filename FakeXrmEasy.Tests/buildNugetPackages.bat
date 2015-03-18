c807d898c5095f7f967adcf5ee27f7aad4d48a8a
jordimontana82 <jordi.montana@gmail.com>
2015-03-18 03:57:19 +0100

    Added install.ps1 script that opens a Getting Starte page.

--- FakeXrmEasy.Tests/buildNugetPackages.bat
+++ FakeXrmEasy.Tests/buildNugetPackages.bat
@@ -1,5 +1,5 @@
 copy ..\FakeXrmEasy\bin\Debug\FakeXrmEasy.dll .\build\lib\net40
 cd build
 nuget pack FakeXrmEasy.dll.nuspec
-nuget push FakeXrmEasy.1.2.12-alpha.nupkg
+nuget push FakeXrmEasy.1.3.1.nupkg
 pause
