f7ed28abe2fa4accf84f54fe82491f24cabb54b4
jordimontana82 <jordi.montana@gmail.com>
2016-02-09 20:28:26 +0100

    Updated NuGet specs

--- FakeXrmEasy.Tests/buildNugetPackages.bat
+++ FakeXrmEasy.Tests/buildNugetPackages.bat
@@ -1,5 +1,5 @@
 copy ..\FakeXrmEasy\bin\Debug\FakeXrmEasy.dll .\build\lib\net40
 cd build
 nuget pack FakeXrmEasy.dll.nuspec
-nuget push FakeXrmEasy.1.8.4.nupkg
+nuget push FakeXrmEasy.1.9.0.nupkg
 pause
