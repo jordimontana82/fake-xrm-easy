1f43b5ab3235dd0962254ca2aee4b12216a5fd47
jordimontana82 <jordi.montana@gmail.com>
2016-06-08 22:01:54 +0200

    Adding OrgServiceContext Sample Test

---
+++ FakeXrmEasy.Tests.Shared/FakeContextTests/OrgServiceContextTests/OrgServiceContextTests.cs
@@ -0,0 +1,32 @@
+﻿using Crm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+
+namespace FakeXrmEasy.Tests.FakeContextTests.OrgServiceContextTests
+{
+    public class OrgServiceContextTests
+    {
+
+        [Fact]
+        public void When_calling_context_add_and_save_changes_entity_is_added_to_the_faked_context()
+        {
+            var context = new XrmFakedContext();
+            var service = context.GetFakedOrganizationService();
+
+            using(var ctx = new XrmServiceContext(service))
+            {
+                ctx.AddObject(new Account() { Name = "Test account" });
+                ctx.SaveChanges();
+
+                var account = ctx.CreateQuery<Account>()
+                            .ToList()
+                            .FirstOrDefault();
+
+                Assert.Equal("Test account", account.Name);
+            }
+        }
+    }
+}
