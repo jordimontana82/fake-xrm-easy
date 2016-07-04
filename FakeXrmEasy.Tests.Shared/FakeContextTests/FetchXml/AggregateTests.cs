768babb94d725d161715c33125c035d05cbab7a7
Sigmund Lahn <sigmund@lahn.no>
2016-07-04 11:28:18 +0200

    More aggregation tests

--- FakeXrmEasy.Tests.Shared/FakeContextTests/FetchXml/AggregateTests.cs
+++ FakeXrmEasy.Tests.Shared/FakeContextTests/FetchXml/AggregateTests.cs
@@ -12,7 +12,7 @@ namespace FakeXrmEasy.Tests.FakeContextTests.FetchXml
     public class AggregateTests
     {
         [Fact]
-        public void FetchXml_Aggregate_Count()
+        public void FetchXml_Aggregate_Group_Count()
         {
 
 
@@ -36,7 +36,7 @@ namespace FakeXrmEasy.Tests.FakeContextTests.FetchXml
             Assert.Equal(3, collection.Entities.Count);
 
             // Make sure we only have the expected properties
-            foreach(var e in collection.Entities)
+            foreach (var e in collection.Entities)
             {
                 Assert.Equal(new[] { "count.contacts", "group.lastname" }, e.Attributes.Keys.OrderBy(x => x));
             }
@@ -49,6 +49,85 @@ namespace FakeXrmEasy.Tests.FakeContextTests.FetchXml
 
             var grantGroup = collection.Entities.SingleOrDefault(x => "Grant".Equals(x.GetAttributeValue<AliasedValue>("group.lastname").Value));
             Assert.Equal(1, grantGroup.GetAttributeValue<AliasedValue>("count.contacts").Value);
+        }
+
+        [Fact]
+        public void FetchXml_Aggregate_CountDistinct()
+        {
+            var fetchXml = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' aggregate='true'>
+                              <entity name='contact'>
+                                    <attribute name='lastname' alias='count' aggregate='count' distinct='true'/>                                    
+                                  </entity>
+                            </fetch>";
+            var ctx = new XrmFakedContext();
+            ctx.Initialize(new[] {
+                new Contact() { Id = Guid.NewGuid(), LastName = "A" },
+                new Contact() { Id = Guid.NewGuid(), LastName = "A" },
+                new Contact() { Id = Guid.NewGuid(), LastName = "A" },
+
+                new Contact() { Id = Guid.NewGuid(), LastName = "B" },
+                new Contact() { Id = Guid.NewGuid(), LastName = "B" },
+
+                new Contact() { Id = Guid.NewGuid(), LastName = "C" },
+            });
+
+            var collection = ctx.GetFakedOrganizationService().RetrieveMultiple(new FetchExpression(fetchXml));
+
+            Assert.Equal(1, collection.Entities.Count);
+            var ent = collection.Entities[0];
+
+            Assert.Equal(3, ent.GetAttributeValue<AliasedValue>("count")?.Value);
+        }
+
+
+        [Fact]
+        public void FetchXml_Aggregate_Sum_Int()
+        {
+            var fetchXml = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' aggregate='true'>
+                              <entity name='contact'>
+                                    <attribute name='numberofchildren' alias='sum' aggregate='sum'/>                                    
+                                  </entity>
+                            </fetch>";
+            var ctx = new XrmFakedContext();
+            ctx.Initialize(new[] {
+                new Contact() { Id = Guid.NewGuid(), NumberOfChildren = 1 },
+                new Contact() { Id = Guid.NewGuid(), NumberOfChildren = 2 },
+                new Contact() { Id = Guid.NewGuid(),  }, /* attribute missing */
+                new Contact() { Id = Guid.NewGuid(), NumberOfChildren = null },
+            });
+
+            var collection = ctx.GetFakedOrganizationService().RetrieveMultiple(new FetchExpression(fetchXml));
+
+            Assert.Equal(1, collection.Entities.Count);
+            var ent = collection.Entities[0];
+
+            Assert.Equal(3, ent.GetAttributeValue<AliasedValue>("sum")?.Value);
+        }
+
+
+        [Fact]
+        public void FetchXml_Aggregate_Sum_Money()
+        {
+            var fetchXml = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' aggregate='true'>
+                              <entity name='salesorderdetail'>
+                                    <attribute name='priceperunit' alias='sum' aggregate='sum'/>                                    
+                                  </entity>
+                            </fetch>";
+            var ctx = new XrmFakedContext();
+            ctx.Initialize(new[] {
+                new SalesOrderDetail() { Id = Guid.NewGuid(), PricePerUnit = new Money(100m) },
+                new SalesOrderDetail() { Id = Guid.NewGuid(), PricePerUnit = new Money(100m)},
+                new SalesOrderDetail() { Id = Guid.NewGuid(),  }, /* attribute missing */
+                new SalesOrderDetail() { Id = Guid.NewGuid(), PricePerUnit = null },
+            });
+
+            var collection = ctx.GetFakedOrganizationService().RetrieveMultiple(new FetchExpression(fetchXml));
+
+            Assert.Equal(1, collection.Entities.Count);
+            var ent = collection.Entities[0];
+
+            Assert.IsType(typeof(Money), ent.GetAttributeValue<AliasedValue>("sum")?.Value);
+            Assert.Equal(200m, (ent.GetAttributeValue<AliasedValue>("sum")?.Value as Money)?.Value);
         }
 
         [Fact]
@@ -65,8 +144,8 @@ namespace FakeXrmEasy.Tests.FakeContextTests.FetchXml
             ctx.Initialize(new[] {
                 new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1980, 1, 1)  },
                 new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1980, 2, 1) },
-                new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1981, 1, 1) },
-                new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1981, 5, 1) },
+                new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1981, 1, 2) },
+                new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1981, 5, 2) },
             });
 
             var collection = ctx.GetFakedOrganizationService().RetrieveMultiple(new FetchExpression(fetchXml));
@@ -93,8 +172,8 @@ namespace FakeXrmEasy.Tests.FakeContextTests.FetchXml
             ctx.Initialize(new[] {
                 new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1980, 1, 1)  },
                 new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1980, 2, 1) },
-                new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1981, 1, 1) },
-                new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1981, 5, 1) },
+                new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1981, 1, 2) },
+                new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1981, 5, 2) },
             });
 
             var collection = ctx.GetFakedOrganizationService().RetrieveMultiple(new FetchExpression(fetchXml));
@@ -106,6 +185,34 @@ namespace FakeXrmEasy.Tests.FakeContextTests.FetchXml
             Assert.Equal(2, byMonth[1].GetAttributeValue<AliasedValue>("count.contacts").Value);
             Assert.Equal(1, byMonth[2].GetAttributeValue<AliasedValue>("count.contacts").Value);
             Assert.Equal(1, byMonth[5].GetAttributeValue<AliasedValue>("count.contacts").Value);
+        }
+
+        [Fact]
+        public void FetchXml_Aggregate_Dategroup_Day()
+        {
+            var fetchXml = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false' aggregate='true'>
+                              <entity name='contact'>
+                                    <attribute name='contactid' alias='count.contacts' aggregate='count' />
+                                    <attribute name='birthdate' alias='group.dob' groupby='true' dategrouping='day' />
+                                  </entity>
+                            </fetch>";
+
+            var ctx = new XrmFakedContext();
+            ctx.Initialize(new[] {
+                new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1980, 1, 1)  },
+                new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1980, 2, 1) },
+                new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1981, 1, 2) },
+                new Contact() { Id = Guid.NewGuid(), BirthDate = new DateTime(1981, 5, 2) },
+            });
+
+            var collection = ctx.GetFakedOrganizationService().RetrieveMultiple(new FetchExpression(fetchXml));
+
+            Assert.Equal(2, collection.Entities.Count);
+            var byDay = collection.Entities.ToDictionary(x => x.GetAttributeValue<AliasedValue>("group.dob").Value as int?);
+            Assert.Equal(new int?[] { 1, 2 }, byDay.Keys.OrderBy(x => x));
+
+            Assert.Equal(2, byDay[1].GetAttributeValue<AliasedValue>("count.contacts").Value);
+            Assert.Equal(2, byDay[2].GetAttributeValue<AliasedValue>("count.contacts").Value);
         }
     }
 }
