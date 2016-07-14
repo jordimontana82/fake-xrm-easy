9c0a96e74113cf82533459a58856b3176dbb203e
jordimontana82 <jordi.montana@gmail.com>
2016-07-15 01:30:16 +0200

    Updated version

--- build.fsx
+++ build.fsx
@@ -38,8 +38,8 @@ let nugetDir                = @".\nuget\"
 let nugetDeployDir          = @"[Enter_NuGet_Url]"
 let packagesDir             = @".\packages\"
 
-let mutable previousVersion = "1.15.0"
-let mutable version         = "1.15.1" //Copy this into previousVersion before publishing packages...
+let mutable previousVersion = "1.15.1"
+let mutable version         = "1.15.2" //Copy this into previousVersion before publishing packages...
 let mutable build           = buildVersion
 let mutable nugetVersion    = version
 let mutable asmVersion      = version
