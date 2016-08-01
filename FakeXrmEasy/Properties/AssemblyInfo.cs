69dd70b2ef63dcb9688be24241449459a35d3408
jordimontana82 <jordi.montana@gmail.com>
2016-08-02 01:51:32 +0200

    Pushed a new package version

--- FakeXrmEasy/Properties/AssemblyInfo.cs
+++ FakeXrmEasy/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.15.4.0")]
-[assembly: AssemblyFileVersion("1.15.4.0")]
-[assembly: AssemblyInformationalVersion("1.15.4.0 - master - 892106")]
+[assembly: AssemblyVersion("1.15.5.0")]
+[assembly: AssemblyFileVersion("1.15.5.0")]
+[assembly: AssemblyInformationalVersion("1.15.5.0 - master - b21e79")]
