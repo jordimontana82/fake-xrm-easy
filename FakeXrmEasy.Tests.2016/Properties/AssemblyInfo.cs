2cf7dc61b8620fa38ed830f41886c5a08df0f4d0
jordimontana82 <jordi.montana@gmail.com>
2016-08-07 17:29:35 +0200

    Updated version

--- FakeXrmEasy.Tests.2016/Properties/AssemblyInfo.cs
+++ FakeXrmEasy.Tests.2016/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.15.5.0")]
-[assembly: AssemblyVersion("1.15.5.0")]
-[assembly: AssemblyFileVersion("1.15.5.0")]
+// [assembly: AssemblyVersion("1.15.6.0")]
+[assembly: AssemblyVersion("1.15.6.0")]
+[assembly: AssemblyFileVersion("1.15.6.0")]
--- FakeXrmEasy/Properties/AssemblyInfo.cs
+++ FakeXrmEasy/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.15.5.0")]
-[assembly: AssemblyFileVersion("1.15.5.0")]
-[assembly: AssemblyInformationalVersion("1.15.5.0 - master - b21e79")]
+[assembly: AssemblyVersion("1.15.6.0")]
+[assembly: AssemblyFileVersion("1.15.6.0")]
+[assembly: AssemblyInformationalVersion("1.15.6.0 - master - faf7e3")]
