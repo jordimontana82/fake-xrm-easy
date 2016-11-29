46d552a27575d2f8a0aa394ab966770a3baf8b76
admin <jordi.montana@gmail.com>
2016-11-30 00:54:25 +0100

    Updated version and added install script for 365 package

--- DevToolkitSamples/Properties/AssemblyInfo.cs
+++ DevToolkitSamples/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.2000.8400")]
+// [assembly: AssemblyVersion("1.19.1.0")]
+[assembly: AssemblyVersion("1.19.1.0")]
+[assembly: AssemblyFileVersion("1.19.1.0")]
