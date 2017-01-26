52024abb95e5d12fbd056dce24549ace39aa9178
amolotkov <molotkov@TOR-W7VSPRO02>
2017-01-26 15:47:38 -0800

    Addedd support for Organization Name in Plugin Context

---
+++ FakeXrmEasy.Tests.Shared/PluginsForTesting/TestContextOrgNamePlugin.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FakeXrmEasy.Tests.PluginsForTesting
+{
+    public class TestContextOrgNamePlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            context.OutputParameters.Add("OrgName", context.OrganizationName);
+
+            
+        }
+    }
+}
