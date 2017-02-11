fdc4b3a710c971def0106a478e43b11d48c1506b
admin <jordi.montana@gmail.com>
2017-02-11 16:47:54 +0100

    Small fix for running the build

--- FakeXrmEasy/Properties/AssemblyInfo.cs
+++ FakeXrmEasy/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.20.1.0")]
-[assembly: AssemblyFileVersion("1.20.1.0")]
-[assembly: AssemblyInformationalVersion("1.20.1.0 - master - c73ac7")]
+[assembly: AssemblyVersion("1.21.0.0")]
+[assembly: AssemblyFileVersion("1.21.0.0")]
+[assembly: AssemblyInformationalVersion("1.21.0.0 - master - de73e7")]
