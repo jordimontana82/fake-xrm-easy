629ff7b944026b97c6f4da9099b10dd49fcceffa
admin <jordi.montana@gmail.com>
2017-02-28 01:47:39 +0100

    Pushed new version

--- build.fsx
+++ build.fsx
@@ -45,8 +45,8 @@ let nugetDeployDir          = @"[Enter_NuGet_Url]"
 let packagesDir             = @".\packages\"
 
 let nuGetCommandLine           = @".\tools\nuget\nuget286.exe"
-let mutable previousVersion = "1.21.1"
-let mutable version         = "1.21.2" //Copy this into previousVersion before publishing packages...
+let mutable previousVersion = "1.21.2"
+let mutable version         = "1.21.3" //Copy this into previousVersion before publishing packages...
 let mutable build           = buildVersion
 let mutable nugetVersion    = version
 let mutable asmVersion      = version
