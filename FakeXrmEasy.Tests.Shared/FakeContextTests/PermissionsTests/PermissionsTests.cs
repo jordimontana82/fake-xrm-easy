e9bede475b64e8d336643e09c8ca5ff90ffd6a5b
Ben Empson <ben@pwp.digital>
2017-04-26 18:02:18 +0200

    #166 Add multiple access rights test

--- FakeXrmEasy.Tests.Shared/FakeContextTests/PermissionsTests/PermissionsTests.cs
+++ FakeXrmEasy.Tests.Shared/FakeContextTests/PermissionsTests/PermissionsTests.cs
@@ -56,6 +56,49 @@ namespace FakeXrmEasy.Tests.FakeContextTests.PermissionsTests
         }
 
         [Fact]
+        public void Entity_Granted_Multiple_Access_Has_Access()
+        {
+            var context = new XrmFakedContext();
+            var contact = new Contact { Id = Guid.NewGuid() };
+            var user = new SystemUser { Id = Guid.NewGuid() };
+
+            context.Initialize(new List<Entity>
+            {
+                contact, user
+            });
+
+            var service = context.GetFakedOrganizationService();
+
+            GrantAccessRequest gar = new GrantAccessRequest
+            {
+                PrincipalAccess = new PrincipalAccess
+                {
+                    AccessMask = AccessRights.ReadAccess | AccessRights.WriteAccess | AccessRights.DeleteAccess | AccessRights.CreateAccess,
+                    Principal = user.ToEntityReference()
+                },
+                Target = contact.ToEntityReference()
+            };
+            service.Execute(gar);
+
+            RetrievePrincipalAccessRequest rpar = new RetrievePrincipalAccessRequest
+            {
+                Target = contact.ToEntityReference(),
+                Principal = user.ToEntityReference()
+            };
+
+            RetrievePrincipalAccessResponse rpaResp = (RetrievePrincipalAccessResponse)service.Execute(rpar);
+            Assert.NotEqual(AccessRights.None, rpaResp.AccessRights);
+            Assert.True(rpaResp.AccessRights.HasFlag(AccessRights.ReadAccess));
+            Assert.False(rpaResp.AccessRights.HasFlag(AccessRights.AppendAccess));
+            Assert.False(rpaResp.AccessRights.HasFlag(AccessRights.AppendToAccess));
+            Assert.False(rpaResp.AccessRights.HasFlag(AccessRights.AssignAccess));
+            Assert.True(rpaResp.AccessRights.HasFlag(AccessRights.CreateAccess));
+            Assert.True(rpaResp.AccessRights.HasFlag(AccessRights.DeleteAccess));
+            Assert.False(rpaResp.AccessRights.HasFlag(AccessRights.ShareAccess));
+            Assert.True(rpaResp.AccessRights.HasFlag(AccessRights.WriteAccess));
+        }
+
+        [Fact]
         public void Entity_Not_Granted_Access_Does_Not_Have_Access()
         {
             var context = new XrmFakedContext();
