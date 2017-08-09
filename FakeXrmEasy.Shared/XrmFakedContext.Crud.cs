3287661a96e5baeecffdf4e30a3ce9395cb98428
Aleksandr Rogov <AleksandrRogov@users.noreply.github.com>
2017-08-09 11:43:55 -0400

    add: simple ootb initializers for invoice and invoicedetails jordimontana82/fake-xrm-easy#149

--- FakeXrmEasy.Shared/XrmFakedContext.Crud.cs
+++ FakeXrmEasy.Shared/XrmFakedContext.Crud.cs
@@ -262,7 +262,7 @@ namespace FakeXrmEasy
 
             var isManyToManyRelationshipEntity = e.LogicalName != null && this.Relationships.ContainsKey(e.LogicalName);
 
-            EntityInitializerService.Initialize(e, CallerId.Id, isManyToManyRelationshipEntity);
+            EntityInitializerService.Initialize(e, CallerId.Id, this, isManyToManyRelationshipEntity);
         }
 
         protected void ValidateEntity(Entity e)
@@ -379,7 +379,6 @@ namespace FakeXrmEasy
             {
                 Data[e.LogicalName].Add(e.Id, e);
             }
-
 
             //Update metadata for that entity
             if (!AttributeMetadataNames.ContainsKey(e.LogicalName))
