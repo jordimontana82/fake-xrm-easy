104cad99c04c95cc9d653737d45f15a1cb7b481c
KD <k.dobrzynski9494@gmail.com>
2017-08-23 14:44:34 +0200

    Added LoseOpportunityRequestExecutor and LoseOpportunityTests

--- FakeXrmEasy.Shared/FakeMessageExecutors/LoseOpportunityRequestExecutor.cs
+++ FakeXrmEasy.Shared/FakeMessageExecutors/LoseOpportunityRequestExecutor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Crm.Sdk.Messages;
 
@@ -13,6 +14,38 @@ namespace FakeXrmEasy.FakeMessageExecutors
 
         public OrganizationResponse Execute(OrganizationRequest request, XrmFakedContext ctx)
         {
+            var req = request as LoseOpportunityRequest;
+
+            // Check if OpportunityClose and Status were passed to request
+            if (req.OpportunityClose != null &&
+                req.Status != null)
+            {
+                // LoseOpportunityRequest.OpportunityClose.OpportunityId
+                var opportunityReference = req.OpportunityClose.GetAttributeValue<EntityReference>("opportunityid");
+                var opportunityId = opportunityReference.Id;
+
+                // Get Opportunities (in good scenario, should return 1 record)
+                var opportunities = (from op in ctx.CreateQuery("opportunity")
+                                     where op.Id == opportunityId
+                                     select op);
+
+                // More than one if to check and give better feedback to user
+                if (opportunities.Count() < 1) throw new Exception(string.Format("No Opportunity found with Id = {0}", opportunityId));
+                else if (opportunities.Count() > 1) throw new Exception(string.Format("More than one Opportunity found with Id = {0}", opportunityId));
+                else
+                {
+                    var opportunity = opportunities.FirstOrDefault();
+                    opportunity.Attributes["statuscode"] = req.Status;
+
+                    ctx.GetOrganizationService().Update(opportunity);
+
+                    return new LoseOpportunityResponse();
+                }
+            }
+            else
+            {
+                throw new Exception("OpportunityClose or Status was not passed to request.");
+            }
         }
 
         public Type GetResponsibleRequestType()
