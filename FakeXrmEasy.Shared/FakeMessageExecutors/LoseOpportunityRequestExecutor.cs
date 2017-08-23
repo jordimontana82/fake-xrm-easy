f5b3170634da249e78ce2630e6baec9810035baa
KD <k.dobrzynski9494@gmail.com>
2017-08-23 14:43:39 +0200

    Added LoseOpportunityRequestExecutor and LoseOpportunityTests

---
+++ FakeXrmEasy.Shared/FakeMessageExecutors/LoseOpportunityRequestExecutor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Crm.Sdk.Messages;
+
+namespace FakeXrmEasy.FakeMessageExecutors
+{
+    public class LoseOpportunityRequestExecutor : IFakeMessageExecutor
+    {
+        public bool CanExecute(OrganizationRequest request)
+        {
+            return request is LoseOpportunityRequest;
+        }
+
+        public OrganizationResponse Execute(OrganizationRequest request, XrmFakedContext ctx)
+        {
+        }
+
+        public Type GetResponsibleRequestType()
+        {
+            return typeof(LoseOpportunityRequest);
+        }
+    }
+}
---
+++ FakeXrmEasy.Tests.Shared/FakeContextTests/LoseOpportunityTests/LoseOpportunityTests.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FakeXrmEasy.Tests.FakeContextTests.LoseOpportunityTests
+{
+    public class LoseOpportunityTests
+    {
+    }
+}
