5a34c9ff7a2b669909db9be96e2809dabab7f915
admin <jordi.montana@gmail.com>
2017-11-05 02:52:21 +0100

    Added test project for v9

--- build.fsx
+++ build.fsx
@@ -48,8 +48,8 @@ let nugetDeployDir          = @"[Enter_NuGet_Url]"
 let packagesDir             = @".\packages\"
 
 let nuGetCommandLine           = @".\tools\nuget\nuget286.exe"
-let mutable previousVersion = "1.37.0"
-let mutable version         = "1.37.1" //Copy this into previousVersion before publishing packages...
+let mutable previousVersion = "1.37.1"
+let mutable version         = "1.37.2" //Copy this into previousVersion before publishing packages...
 let mutable build           = buildVersion
 let mutable nugetVersion    = version
 let mutable asmVersion      = version
