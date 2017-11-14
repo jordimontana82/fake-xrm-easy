35c258750331ca6c794b175613b9f698aa583c97
admin <jordi.montana@gmail.com>
2017-11-14 19:46:37 +0100

    Updated version

--- Install-scripts/2015/Install.ps1
+++ Install-scripts/2015/Install.ps1
@@ -1,4 +1,4 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-$DTE.ItemOperations.Navigate("http://dynamicsvalue.com/get-started/nuget-install-2015?version=1.37.2")
+$DTE.ItemOperations.Navigate("http://dynamicsvalue.com/get-started/nuget-install-2015?version=1.37.3")
 
