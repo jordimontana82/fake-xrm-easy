f9abc64bb69e8706e3102161cfbdf1a54256dc13
Jamie Bowman <mrjamiebowman@gmail.com>
2018-06-14 20:32:08 -0400

    Test that methods add Pre/Post EntityImageCollection to Context before executing plugin.

--- FakeXrmEasy.Tests.Shared/FakeContextTests/FakeContextTestPlugins.cs
+++ FakeXrmEasy.Tests.Shared/FakeContextTests/FakeContextTestPlugins.cs
@@ -302,17 +302,53 @@ namespace FakeXrmEasy.Tests
         }
 
         [Fact]
-        public void When_passing_postentityimages_it_get_added_to_context()
+        public void When_passing_preentityimages_it_gets_added_to_context()
         {
+            // arrange
+            var context = new XrmFakedContext();
+            var target = new Entity();
+
+            EntityImageCollection preEntityImages = new EntityImageCollection();
+            preEntityImages.Add("PreImage", new Entity());
+
+            // act
+            context.ExecutePluginWithTargetAndPreEntityImages<EntityImagesPlugin>(target, preEntityImages);
+
+            // assert
+            EntityImageCollection postImagesReturned = target["PostEntityImages"] as EntityImageCollection;
+
+            if (postImagesReturned.Count > 0)
+                throw new Exception("PostEntityImages should not be set.");
+
+            EntityImageCollection preImagesReturned = target["PreEntityImages"] as EntityImageCollection;
+
+            Assert.Equal(1, preImagesReturned?.Count);
+            Assert.IsType(typeof(Entity), preImagesReturned["PreImage"]);
+        }
+
+        [Fact]
+        public void When_passing_postentityimages_it_gets_added_to_context()
+        {
+            // arrange
             var context = new XrmFakedContext();
             var target = new Entity();
 
             EntityImageCollection postEntityImages = new EntityImageCollection();
             postEntityImages.Add("PostImage", new Entity());
 
-            // TODO: assert post entity image collection.
+            // act
+            context.ExecutePluginWithTargetAndPostEntityImages<EntityImagesPlugin>(target, postEntityImages);
 
-            context.ExecutePluginWithTargetAndPostEntityImages<DoNothingPlugin>(target, postEntityImages);
+            // assert
+            EntityImageCollection preImagesReturned = target["PreEntityImages"] as EntityImageCollection;
+
+            if (preImagesReturned.Count > 0)
+                throw new Exception("PreImages should not be set.");
+
+            EntityImageCollection postImagesReturned = target["PostEntityImages"] as EntityImageCollection;
+
+            Assert.Equal(1, postImagesReturned?.Count);
+            Assert.IsType(typeof(Entity), postImagesReturned["PostImage"]);
         }
     }
 }
