100deac0a5abad4bf91ec60c3c8a6f76e14a40da
QLDRA\CahillD <dale.cahill@qldra.org.au>
2018-07-25 12:01:16 +1000

    Test - Ensure correct operation after fix

---
+++ FakeXrmEasy.Tests.Shared/PluginsForTesting/PostOperationUpdatePlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Crm;
+using Microsoft.Xrm.Sdk;
+
+namespace FakeXrmEasy.Tests.PluginsForTesting
+{
+    public class PostOperationUpdatePlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var entity = (Entity)context.InputParameters["Target"];
+
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(null);
+
+            var updatedEntity = new Account { Id = entity.Id, Name = "Updated" };
+            service.Update(updatedEntity);
+        }
+    }
+}
