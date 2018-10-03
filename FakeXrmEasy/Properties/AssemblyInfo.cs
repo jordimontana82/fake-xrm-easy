aa064d9771ab70c2edb2804dd5a15b5ceca323a5
Betim Beja <betimbeja@users.noreply.github.com>
2018-10-03 11:34:51 +0200

    Polishing IXrmContext - Removed unused parameters and deprecated some old ExecutePluginWith* methods. - Removed the generic type on methods that accept a plugin instance since the type wasn't used - Copied some documentation from IXrmContext to XrmFakedContext because intellisense works based on the type

--- FakeXrmEasy/Properties/AssemblyInfo.cs
+++ FakeXrmEasy/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.47.0.0")]
 [assembly: AssemblyFileVersion("1.47.0.0")]
-[assembly: AssemblyInformationalVersion("1.47.0.0 - master - 2c4d88")]
+[assembly: AssemblyInformationalVersion("1.47.0.0 - XrmContext-Polish - ecf66f")]
