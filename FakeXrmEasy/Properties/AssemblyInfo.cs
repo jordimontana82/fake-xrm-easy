a16f0bf9d469519a23bc4622db5e6246f01af80c
admin <jordi.montana@gmail.com>
2018-10-09 15:29:28 +0200

    Updated version

--- FakeXrmEasy/Properties/AssemblyInfo.cs
+++ FakeXrmEasy/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.47.0.0")]
-[assembly: AssemblyFileVersion("1.47.0.0")]
-[assembly: AssemblyInformationalVersion("1.47.0.0 - XrmContext-Polish - ecf66f")]
+[assembly: AssemblyVersion("1.47.1.0")]
+[assembly: AssemblyFileVersion("1.47.1.0")]
+[assembly: AssemblyInformationalVersion("1.47.1.0 - master - b1b9ed")]
