dcadb7d2d144969728fba36c68661e187b815678
Phuoc.Le <vanphuoc@gmail.com>
2019-02-12 09:25:44 +0700

    + Implementation SendEmailRequestExecutor

---
+++ FakeXrmEasy.Shared/FakeMessageExecutors/SendEmailRequestExecutor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+
+namespace FakeXrmEasy.FakeMessageExecutors
+{
+    public class SendEmailRequestExecutor : IFakeMessageExecutor
+    {
+        public bool CanExecute(OrganizationRequest request)
+        {
+            return request is SendEmailRequest;
+        }
+
+        public OrganizationResponse Execute(OrganizationRequest request, XrmFakedContext ctx)
+        {
+            var req = request as SendEmailRequest;
+#if FAKE_XRM_EASY || FAKE_XRM_EASY_2013
+            var entity = new Entity("email");
+            entity.Id = req.EmailId;
+#else
+            var entity = new Entity("email", req.EmailId);
+#endif
+            entity["statecode"] = new OptionSetValue(1); //Completed
+            entity["statuscode"] = new OptionSetValue(3); //Sent
+            ctx.GetOrganizationService().Update(entity);
+            return new SendEmailResponse();
+        }
+
+        public Type GetResponsibleRequestType()
+        {
+            return typeof(SendEmailRequest);
+        }
+    }
+}
---
+++ FakeXrmEasy.Tests.Shared/FakeContextTests/SendEmailRequestTests/SendEmailRequestTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace FakeXrmEasy.Tests.FakeContextTests.SendEmailRequestTests
+{
+    public class SendEmailRequestTests
+    {
+        [Fact]
+        public void When_SendEmailRequest_call_statecode_is_Completed_and_statuscode_is_Sent()
+        {
+            var context = new XrmFakedContext();
+            context.ProxyTypesAssembly = Assembly.GetExecutingAssembly();
+            var service = context.GetOrganizationService();
+
+            var email = new Crm.Email()
+            {
+                Id = Guid.NewGuid()
+            };
+            var emailId = service.Create(email);
+
+            var request = new SendEmailRequest
+            {
+                EmailId = emailId,
+                TrackingToken = "TrackingToken",
+                IssueSend = true
+            };
+            var response = (SendEmailResponse)service.Execute(request);
+
+            var entity = service.Retrieve("email", emailId, new ColumnSet("statecode", "statuscode"));
+            Assert.Equal(1, entity?.GetAttributeValue<OptionSetValue>("statecode")?.Value);
+            Assert.Equal(3, entity?.GetAttributeValue<OptionSetValue>("statuscode")?.Value);
+        }
+    }
+}
