c0413bf1c18a2ea5635460c3dfca3859a8146693
Andy Elsmore <elsmore.andy@gmail.com>
2019-04-01 13:10:28 +0100

    Add UpsertRequest

---
+++ FakeXrmEasy.Shared/FakeMessageExecutors/UpsertRequestExecutor.cs
@@ -0,0 +1,38 @@
+﻿#if FAKE_XRM_EASY_2015 || FAKE_XRM_EASY_2016 || FAKE_XRM_EASY_365 || FAKE_XRM_EASY_9
+
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using System;
+
+namespace FakeXrmEasy.FakeMessageExecutors
+{
+
+    public class UpsertRequestExecutor : IFakeMessageExecutor
+    {
+        public bool CanExecute(OrganizationRequest request)
+        {
+            return request is UpsertRequest;
+        }
+
+        public OrganizationResponse Execute(OrganizationRequest request, XrmFakedContext ctx)
+        {
+            var upsertRequest = (UpsertRequest)request;
+
+            var target = (Entity)request.Parameters["Target"];
+
+            var service = ctx.GetOrganizationService();
+            service.Update(target);
+
+            return new UpsertResponse()
+            {
+                
+            };
+        }
+
+        public Type GetResponsibleRequestType()
+        {
+            return typeof(UpsertRequest);
+        }
+    }
+}
+#endif
