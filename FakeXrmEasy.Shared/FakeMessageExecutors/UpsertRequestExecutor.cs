5f6595f91ebe293eca106b6b7263b78e1ebd62f8
Betim Beja <betimbeja@users.noreply.github.com>
2019-04-02 23:46:03 +0200

    Implemented UpsertRequestExecutor

---
+++ FakeXrmEasy.Shared/FakeMessageExecutors/UpsertRequestExecutor.cs
@@ -0,0 +1,48 @@
+﻿using FakeXrmEasy.Extensions;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using System;
+
+#if !FAKE_XRM_EASY && !FAKE_XRM_EASY_2013 && !FAKE_XRM_EASY_2015
+
+namespace FakeXrmEasy.FakeMessageExecutors
+{
+    public class UpsertRequestExecutor : IFakeMessageExecutor
+    {
+        public bool CanExecute(OrganizationRequest request)
+        {
+            return request is UpsertRequest;
+        }
+
+        public OrganizationResponse Execute(OrganizationRequest request, XrmFakedContext ctx)
+        {
+            var upsertRequest = (UpsertRequest)request;
+            bool recordCreated;
+
+            var service = ctx.GetOrganizationService();
+
+            if (ctx.Data.ContainsKey(upsertRequest.Target.LogicalName) &&
+                ctx.Data[upsertRequest.Target.LogicalName].ContainsKey(upsertRequest.Target.Id))
+            {
+                recordCreated = false;
+                service.Update(upsertRequest.Target);
+            }
+            else
+            {
+                recordCreated = true;
+                service.Create(upsertRequest.Target);
+            }
+            
+            var result = new UpsertResponse();
+            result.Results.Add("RecordCreated", recordCreated);
+            result.Results.Add("Target", upsertRequest.Target.ToEntityReference());
+            return result;
+        }
+
+        public Type GetResponsibleRequestType()
+        {
+            return typeof(UpsertRequest);
+        }
+    }
+}
+#endif
---
+++ FakeXrmEasy.Tests.Shared/FakeContextTests/UpsertRequestTests/Tests.cs
@@ -0,0 +1,69 @@
+﻿using Crm;
+using Microsoft.Xrm.Sdk.Messages;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace FakeXrmEasy.Tests.FakeContextTests.UpsertRequestTests
+{
+#if !FAKE_XRM_EASY && !FAKE_XRM_EASY_2013 && !FAKE_XRM_EASY_2015
+    public class Tests
+    {
+        [Fact]
+        public void Upsert_Creates_Record_When_It_Does_Not_Exist()
+        {
+            var context = new XrmFakedContext();
+            context.ProxyTypesAssembly = Assembly.GetExecutingAssembly();
+            var service = context.GetOrganizationService();
+            
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "FakeXrm",
+                LastName = "Easy"
+            };
+
+            var request = new UpsertRequest()
+            {
+                Target = contact
+            };
+
+            var response = (UpsertResponse)service.Execute(request);
+
+            Assert.Equal(true, response.RecordCreated);
+        }
+
+        [Fact]
+        public void Upsert_Updates_Record_When_It_Exists()
+        {
+            var context = new XrmFakedContext();
+            context.ProxyTypesAssembly = Assembly.GetExecutingAssembly();
+            var service = context.GetOrganizationService();
+
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid()
+            };
+            context.Initialize(new[] { contact });
+
+            contact = new Contact()
+            {
+                Id = contact.Id,
+                FirstName = "FakeXrm",
+                LastName = "Easy"
+            };
+
+            var request = new UpsertRequest()
+            {
+                Target = contact
+            };
+
+            var response = (UpsertResponse)service.Execute(request);
+
+            Assert.Equal(false, response.RecordCreated);
+        }
+    }
+#endif
+}
