633d9f7c0f1452d70438000eb4713c4ff2c0a395
Betim Beja <betimbeja@users.noreply.github.com>
2019-07-03 19:21:03 +0200

    Fixed check for MetadataId parameter

--- FakeXrmEasy.Shared/FakeMessageExecutors/RetrieveOptionSetRequestExecutor.cs
+++ FakeXrmEasy.Shared/FakeMessageExecutors/RetrieveOptionSetRequestExecutor.cs
@@ -15,10 +15,10 @@ namespace FakeXrmEasy.FakeMessageExecutors
         {
             var retrieveOptionSetRequest = (RetrieveOptionSetRequest)request;
 
-			if (retrieveOptionSetRequest.Parameters.ContainsKey("MetadataId")) //ToDo: Implement retrieving option sets by Id
-			{
-				FakeOrganizationServiceFault.Throw(ErrorCodes.ObjectDoesNotExist, $"Could not find optionset with optionset id: {retrieveOptionSetRequest.MetadataId}");
-			}
+            if (retrieveOptionSetRequest.MetadataId != Guid.Empty) //ToDo: Implement retrieving option sets by Id
+            {
+                FakeOrganizationServiceFault.Throw(ErrorCodes.ObjectDoesNotExist, $"Could not find optionset with optionset id: {retrieveOptionSetRequest.MetadataId}");
+            }
 
             var name = retrieveOptionSetRequest.Name;
 
@@ -27,9 +27,9 @@ namespace FakeXrmEasy.FakeMessageExecutors
                 FakeOrganizationServiceFault.Throw(ErrorCodes.InvalidArgument, "Name is required when optionSet id is not specified");
             }
 
-			if (!ctx.OptionSetValuesMetadata.ContainsKey(name))
+            if (!ctx.OptionSetValuesMetadata.ContainsKey(name))
             {
-				FakeOrganizationServiceFault.Throw(ErrorCodes.ObjectDoesNotExist, string.Format("An OptionSetMetadata with the name {0} does not exist.", name));
+                FakeOrganizationServiceFault.Throw(ErrorCodes.ObjectDoesNotExist, string.Format("An OptionSetMetadata with the name {0} does not exist.", name));
             }
 
             var optionSetMetadata = ctx.OptionSetValuesMetadata[name];
--- FakeXrmEasy.Shared/FakeOrganizationServiceFault.cs
+++ FakeXrmEasy.Shared/FakeOrganizationServiceFault.cs
@@ -7,7 +7,7 @@ namespace FakeXrmEasy
     {
         public static void Throw(ErrorCodes errorCode, string message)
         {
-            throw new FaultException<OrganizationServiceFault>(new OrganizationServiceFault() { ErrorCode = (int)errorCode, Message = message });
+            throw new FaultException<OrganizationServiceFault>(new OrganizationServiceFault() { ErrorCode = (int)errorCode, Message = message }, message);
         }
     }
 }
