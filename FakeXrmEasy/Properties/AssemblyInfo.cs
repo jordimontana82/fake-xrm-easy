a19cfdb8b48ec97bb32259e9b7428400e6d0c287
Jordi <jordi.montana@gmail.com>
2020-01-30 03:55:43 +0100

    Pushed a new version and updated FakeXrmEeasy's logo yeah!

--- FakeXrmEasy/Properties/AssemblyInfo.cs
+++ FakeXrmEasy/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.56.0.0")]
-[assembly: AssemblyFileVersion("1.56.0.0")]
-[assembly: AssemblyInformationalVersion("1.56.0.0 - master - c0b19a")]
+[assembly: AssemblyVersion("1.57.0.0")]
+[assembly: AssemblyFileVersion("1.57.0.0")]
+[assembly: AssemblyInformationalVersion("1.57.0.0 - master - 6b7fc5")]
