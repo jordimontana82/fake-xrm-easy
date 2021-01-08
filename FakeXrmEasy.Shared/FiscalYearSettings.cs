6477348fa44901c68441d6b473c82a111713fd6a
Charles Oliner <chaoliner@gmail.com>
2021-01-08 13:22:02 -0500

    Added fiscalyeardisplaycode to FiscalYearSettings

--- FakeXrmEasy.Shared/FiscalYearSettings.cs
+++ FakeXrmEasy.Shared/FiscalYearSettings.cs
@@ -21,5 +21,31 @@ namespace FakeXrmEasy
             Monthly = 2003,
             FourWeek = 2004
         }
+
+        [AttributeLogicalName("fiscalyeardisplaycode")]
+        public FiscalYearDisplayCode FiscalYearDisplay { get; set; }
+
+        public enum FiscalYearDisplayCode
+        {
+            StartDate = 1,
+            EndDate = 2
+        }
+
+        public static void GetFiscalYearStartAndEndDate(FiscalYearSettings fiscalYearSettings, int year, out DateTime startDate, out DateTime endDate)
+        {
+            var basicStartDate = fiscalYearSettings?.StartDate ?? new DateTime(DateTime.Today.Year, 4, 1);
+
+            if (fiscalYearSettings?.FiscalYearDisplay == FiscalYearDisplayCode.EndDate)
+            {
+                DateTime basicEndDate = basicStartDate.AddYears(1).AddDays(-1);
+                endDate = new DateTime(year, basicEndDate.Month, basicEndDate.Day);
+                startDate = endDate.AddYears(-1).AddDays(1);
+            }
+            else
+            {
+                startDate = new DateTime(year, basicStartDate.Month, basicStartDate.Day);
+                endDate = startDate.AddYears(1).AddDays(-1);
+            }
+        }
     }
 }
