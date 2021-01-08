f00f7168037332b00fa6641bfc22a625a22d81d8
Victor Sanchez <v.sanchez@AXAZURE.COM>
2021-01-08 21:43:52 +0100

    Added DeleteRecordChangeHistoryRequest Fake executor for Shared project. Added DeleteRecordChangeHistoryRequestTest Fake executor for Test Shared project.

---
+++ FakeXrmEasy.Shared/FakeMessageExecutors/DeleteRecordChangeHistoryRequestExecutor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Crm.Sdk.Messages;
+using System;
+using System.ServiceModel;
+namespace FakeXrmEasy.FakeMessageExecutors
+{
+    public class DeleteRecordChangeHistoryRequestExecutor : IFakeMessageExecutor
+    {
+
+        public bool CanExecute(OrganizationRequest request)
+        {
+            return request is DeleteRecordChangeHistoryRequest;
+        }
+
+        public OrganizationResponse Execute(OrganizationRequest request, XrmFakedContext ctx)
+        {
+            var req = request as DeleteRecordChangeHistoryRequest;
+
+            var res = new DeleteRecordChangeHistoryResponse();
+
+            return res;
+        }
+
+        public Type GetResponsibleRequestType()
+        {
+            return typeof(DeleteRecordChangeHistoryRequest);
+        }
+    }
+
+}
